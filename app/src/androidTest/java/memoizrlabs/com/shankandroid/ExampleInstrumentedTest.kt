c54d19628b0ed6a86fbd8de805c7a6be8bff3233
memoizr <memoizrlabs@gmail.com>
2019-04-30 21:27:31 +0100

    Update gradle file

--- app/src/androidTest/java/memoizrlabs/com/shankandroid/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/memoizrlabs/com/shankandroid/ExampleInstrumentedTest.kt
@@ -3,8 +3,8 @@ package memoizrlabs.com.shankandroid
 import android.os.Debug
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import life.shank.ShankModule
-import life.shank.new
 import life.shank.android.AppContextModule
+import life.shank.new
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -14,25 +14,31 @@ class ExampleInstrumentedTest {
 //    @Rule
 //    var repeatRule = RepeatRule()
 
+    class xx {
+        @JvmField
+        val hashCode = hashCode()
+    }
     @Test
     fun useAppContext() {
+        val a = xx()
         // Context of the app under test.
         Debug.startMethodTracing("sample")
+        Module.a2()
         Module.a2()
         AppContextModule.appContext()
         Debug.stopMethodTracing()
     }
 }
 
-val s = "dlfjdlkjf"
+@JvmField val s = "dlfjdlkjf"
 
 object Module: ShankModule {
-    val a2 = new { -> s }
-    val a3 = new { -> s }
-    val a4 = new { -> s }
-    val a5 = new { -> s }
-    val a6 = new { -> s }
-    val a7 = new { -> s }
-    val a8 = new { -> s }
+    @JvmField val a2 = new { -> s }
+    @JvmField val a3 = new { -> s }
+    @JvmField val a4 = new { -> s }
+    @JvmField val a5 = new { -> s }
+    @JvmField val a6 = new { -> s }
+    @JvmField val a7 = new { -> s }
+    @JvmField val a8 = new { -> s }
 }
 
